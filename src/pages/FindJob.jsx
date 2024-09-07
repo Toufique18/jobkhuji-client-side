a790ce7b3b7f542d6ec49c7ea8ec0897e0dc55af
Toufique18 <toufiqueeamam7@gmail.com>
2024-09-07 21:30:08 +0600

    search feature in findjob page and commit

--- src/pages/FindJob.jsx
+++ src/pages/FindJob.jsx
@@ -5,17 +5,18 @@ const FindJob = () => {
     const [jobs, setJobs] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchJobs = async () => {
-            const response = await fetch(`http://localhost:5000/jobs?page=${currentPage}&limit=12`);
+            const response = await fetch(`http://localhost:5000/jobs?page=${currentPage}&limit=12&search=${searchTerm}`);
             const data = await response.json();
             setJobs(data.jobs);
             setTotalPages(data.totalPages);
         };
 
         fetchJobs();
-    }, [currentPage]);
+    }, [currentPage, searchTerm]);
 
     const handlePageChange = (page) => {
         if (page > 0 && page <= totalPages) {
@@ -23,9 +24,27 @@ const FindJob = () => {
         }
     };
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+        setCurrentPage(1);
+    };
+
     return (
         <div className="container mx-auto lg:px-12 px-5 py-5">
             <h1 className="text-2xl font-bold mb-4">Find Jobs</h1>
+            <div>
+            <div className="mb-4 lg:flex flex-1">
+                <input
+                    type="text"
+                    className="input input-bordered w-full mb-2"
+                    placeholder="Search jobs..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                
+                
+            </div>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {jobs.map((card) => (
                     <Card key={card._id} card={card} />
